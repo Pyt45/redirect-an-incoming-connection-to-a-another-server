ed954ea6f2568a2f5255d32318710f639eeeeec0
Pyt45 <hossamaqlzim107@gmail.com>
2020-09-20 13:42:15 +0100

    Add firewall script

--- srcs/firewall.sh
+++ srcs/firewall.sh
@@ -6,3 +6,11 @@
 #Authors        :Ayoub Aqlzim + yassin slati(aka Dolfine)
 #License       : NO_LICENSE	/ Open source
 #######################################################################################
+
+sudo echo "1" > /proc/sys/net/ipv4/ip_forward
+
+#for a specifi ip_address
+iptables -t nat -A PREROUTING -s ip_phone_address -p tcp --dport 80 -j DNAT --to-destination ip_private:80
+
+#for the hole network
+iptables -t nat -A PREROUTING -s ip_network/24 -p tcp --dport 80 -j DNAT --to-destination ip_private:80
